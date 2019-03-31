7fa97c47d8c1b37fef5ede70c3697dfa9ac0eb11
mahume <michael.a.hume@gmail.com>
2019-03-31 13:32:32 -0600

    update

---
+++ public/assets/js/app.js
@@ -0,0 +1,48 @@
+$(function() {
+    $('.change-devoured').on('click', event => {
+        let id = $(this).data('id')
+        let newDevoured = $(this).data('new-devoured')
+
+        let newDevouredState = {
+            devoured: newDevoured
+        }
+
+        $.ajax(`/api/burgers/${id}`, {
+            type: 'PUT',
+            data: newDevouredState
+        }).then(
+            () => {
+                console.log(`Changed devoured to ${newDevoured}`)
+                location.reload()
+            }
+        )
+    })
+    $('.create-form').on('submit', event => {
+        event.preventDefault()
+        let newBurger = {
+            name: $('#burger').val().trim(),
+            devoured: $('[name=devoured]:checked').val().trim()
+        }
+        $.ajax('/api/burgers', {
+            type: 'POST',
+            data: newBurger
+        }).then(
+            () => {
+                console.log('Created new burger')
+                location.reload()
+            }
+        )
+    })
+    $('.delete-burger').on('click', event => {
+        let id = $(this).data('id')
+        $.ajax(`api/burgers/${id}`, {
+            type: 'DELETE'
+        }).then(
+            () => {
+                console.log(`Deleted burger ${id}`)
+                location.reload()
+            }
+        )
+    })
+
+})
